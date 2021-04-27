a89005d526647e265833ffa719ed8bfe1c2740b2
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-04-27 13:56:26 -0300

    Adicionando exercício 2

--- exercises/introducao-a-react_1/introduction-to-react/src/App.js
+++ exercises/introducao-a-react_1/introduction-to-react/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 
+const arrayList = ['Efetuando o primerio exercício', 'Me aprimorando com o exercício 2', 'Chegando no exercício 3'];
+
 const task = (value) => {
   return (
     <li>{value}</li>
   );
 }
 
+const taskList = (array) => {
+  return array.map((element) => task(element));
+};
+
 function App() {
   return (
-    <ol>
-      {task('Efetuando o primeiro exercício;')}
-    </ol>
+    <ul>
+      {taskList(arrayList)}
+    </ul>
   );
 }
 
